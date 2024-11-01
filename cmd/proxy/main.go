6b2f35023ab0d68afbd45d673bb414a6721cf0f5
Igor <SandQuattro@gmail.com>
2024-11-01 20:29:57 +0300

    chore: added zerologger with context

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"simple-proxy-server/internal/proxy"
+	zerologger "simple-proxy-server/pkg/logger"
 	"strings"
+	"syscall"
+
+	"github.com/rs/zerolog"
 )
 
 var domainList []string
@@ -33,31 +40,90 @@ func init() {
 }
 
 func main() {
-	// Listen for incoming connections on port 8080
-	listener, err := net.Listen("tcp", ":8080")
+	zerologger.InitLogger(true)
+	logger := zerologger.GetCtxLogger(context.Background())
+	err := run(logger)
 	if err != nil {
-		log.Fatalln("Failed to listen on port 8080:", err)
-	}
-	log.Println("Proxy server listening on port 8080")
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println("Failed to accept connection:", err)
-			continue
-		}
-
-		// process our new connections concurrently
-		go handleLoop(conn)
+		logger.Fatal().Err(err).Msg("Error running the server")
 	}
 }
 
-func handleLoop(conn net.Conn) {
+func run(logger zerolog.Logger) error {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+
+	err := proxy.SetOsProxy(8080)
+	if err != nil {
+		logger.Error().Err(err).Msg("error set system-wide proxy")
+		return err
+	}
+
+	logger.Info().Msg("system-wide proxy was set successfully")
+
+	// Listen for incoming connections on port 8080
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to listen on port 8080")
+	}
+	logger.Info().Msg("Proxy server listening on port 8080")
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				// останавливаем горутину
+				return
+			default:
+				conn, err := listener.Accept()
+				if err != nil {
+					logger.Println("Failed to accept connection:", err)
+					continue
+				}
+
+				// process our new connections concurrently
+				go handleLoop(ctx, conn)
+			}
+		}
+	}()
+
+	stopped := make(chan struct{})
+	go func() {
+		// Используем буферизированный канал, как рекомендовано внутри signal.Notify функции
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Блокируемся и ожидаем из канала quit - interrupt signal,
+		// чтобы сделать gracefully shutdown с таймаутом в 10 сек
+		<-quit
+
+		// Завершаем работу горутин
+		cancelFunc()
+
+		err := proxy.UnsetOsProxy()
+		if err != nil {
+			logger.Error().Err(err).Msg("error unset system-wide proxy")
+		}
+		logger.Info().Msg("system-wide proxy was unset successfully")
+
+		close(stopped)
+	}()
+
+	<-stopped
+
+	logger.Info().Msg("Server gracefully stopped, bye, bye!")
+
+	return nil
+}
+
+func handleLoop(ctx context.Context, conn net.Conn) {
 	defer conn.Close()
+
+	logger := zerologger.GetCtxLogger(ctx)
+
 	reader := bufio.NewReader(conn)
 	req, err := http.ReadRequest(reader)
 	if err != nil {
-		log.Printf("Error reading request: %v", err)
+		logger.Printf("Error reading request: %v", err)
 		return
 	}
 
@@ -71,28 +137,31 @@ func handleLoop(conn net.Conn) {
 		}
 	}
 
-	if processBlockedHosts(conn, req) {
+	if processBlockedHosts(ctx, conn, req) {
 		return
 	}
 
 	// this is a request to start proxy tunnel
 	if req.Method == "CONNECT" {
 		// Handle HTTPS tunneling
-		log.Printf("Establishing tunnel to %s", host)
+		logger.Printf("Establishing tunnel to %s", host)
 		conn.Write([]byte("HTTP/1.1 200 Connection Established\r\n\r\n"))
-		handleTunneling(conn, host)
+		handleTunneling(ctx, conn, host)
 	} else {
 		// Handle regular HTTP requests
-		log.Printf("Handling HTTP request for %s", req.URL)
-		handleHTTP(conn, req)
+		logger.Printf("Handling HTTP request for %s", req.URL)
+		handleHTTP(ctx, conn, req)
 	}
 }
 
-func handleTunneling(clientConn net.Conn, destHost string) {
+func handleTunneling(ctx context.Context, clientConn net.Conn, destHost string) {
 	defer clientConn.Close()
+
+	logger := zerologger.GetCtxLogger(ctx)
+
 	remoteConn, err := net.Dial("tcp", destHost)
 	if err != nil {
-		log.Printf("Failed to connect to host %s: %v", destHost, err)
+		logger.Printf("Failed to connect to host %s: %v", destHost, err)
 		return
 	}
 	defer remoteConn.Close()
@@ -104,8 +173,10 @@ func handleTunneling(clientConn net.Conn, destHost string) {
 	io.Copy(clientConn, remoteConn)
 }
 
-func handleHTTP(clientConn net.Conn, req *http.Request) {
-	log.Printf("handling request, scheme:%s\n", req.URL.Scheme)
+func handleHTTP(ctx context.Context, clientConn net.Conn, req *http.Request) {
+	logger := zerologger.GetCtxLogger(ctx)
+
+	logger.Printf("handling request, scheme:%s\n", req.URL.Scheme)
 
 	transport := &http.Transport{}
 	defer clientConn.Close()
@@ -122,7 +193,7 @@ func handleHTTP(clientConn net.Conn, req *http.Request) {
 	// Forward the request to the actual remote host
 	resp, err := transport.RoundTrip(req)
 	if err != nil {
-		log.Printf("Error forwarding request: %v, err:%v", req, err)
+		logger.Printf("Error forwarding request: %v, err:%v", req, err)
 		return
 	}
 	defer resp.Body.Close()
@@ -130,15 +201,17 @@ func handleHTTP(clientConn net.Conn, req *http.Request) {
 	// Write the response back to the client
 	err = resp.Write(clientConn)
 	if err != nil {
-		log.Printf("Error writing response to client: %v", err)
+		logger.Printf("Error writing response to client: %v", err)
 	}
 }
 
-func processBlockedHosts(conn net.Conn, req *http.Request) bool {
+func processBlockedHosts(ctx context.Context, conn net.Conn, req *http.Request) bool {
+	logger := zerologger.GetCtxLogger(ctx)
+
 	// blocking advertising domains
 	for _, domain := range domainList {
 		if strings.Contains(req.Host, domain) {
-			log.Printf("Blocking access to %s", req.Host)
+			logger.Printf("Blocking access to %s", req.Host)
 
 			if req.Method == "CONNECT" {
 				// Respond with 403 Forbidden for HTTPS sites
